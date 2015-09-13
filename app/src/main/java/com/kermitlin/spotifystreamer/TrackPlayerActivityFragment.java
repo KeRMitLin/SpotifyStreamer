99d95b5c7572703c64e702c0d6790a2cd4673079
Angus <jostar1620@gmail.com>
2015-09-13 19:33:10 +0800

    test

--- app/src/main/java/com/kermitlin/spotifystreamer/TrackPlayerActivityFragment.java
+++ app/src/main/java/com/kermitlin/spotifystreamer/TrackPlayerActivityFragment.java
@@ -39,6 +39,7 @@ public class TrackPlayerActivityFragment extends DialogFragment {
     static Button bt_Play;
     static Button bt_Pause;
     private Button bt_Next;
+    //test
 
     public interface Callback {
         /**
